e5c936278bcca1e76858f23fd532be1af123c58c
Adl <issayev.adilet@gmail.com>
2018-11-12 12:03:28 +0600

    changed swipe for btn

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -109,46 +109,66 @@
   @foreach($modes as $k => $mode)
     <section class="modes-products">
       <div class="container">
-        <h2>{{ $mode->title }}</h2><br>
-        <div class="xs-tab-slider owl-carousel -row category-v4">
-          @foreach($mode->products->where('status', 1)->take(16)->chunk(2) as $chunk)
-            <div class="xs-tab-slider-item">
-              @foreach($chunk as $product)
-                <div class="xs-product-wraper version-3 text-center">
-                  <a href="/goods/{{ $product->id.'-'.$product->slug }}">
-                    <img src="/img/products/{{ $product->path.'/'.$product->image }}" alt="{{ $product->title }}">
-                  </a>
-                  <div class="offer">
-                    @foreach($product->modes as $m)
-                      @if(in_array($m->slug, ['novelty', 'best-price', 'stock', 'plus-gift']))
-                        <div class="offer-{{ $m->slug }}">{{ $m->title }}</div>
-                      @endif
-                    @endforeach
-                  </div>
-                  <div class="xs-product-content">
-                    <h4 class="product-title"><a href="/goods/{{ $product->id.'-'.$product->slug }}">{{ $product->title }}</a></h4>
-                    <span class="price"><b>{{ $product->price }}〒</b></span>
-
-                    @if (is_array($favorites) AND in_array($product->id, $favorites['products_id']))
-                      <button type="button" class="btn btn-dark btn-compact m-10 btn-sm" data-favorite-id="{{ $product->id }}" onclick="toggleFavorite(this);" title="Добавлено в избранные"><span class="icon icon-heart h5"></span></button>
-                    @else
-                      <button type="button" class="btn btn-outline-primary btn-compact m-10 btn-sm" data-favorite-id="{{ $product->id }}" onclick="toggleFavorite(this);" title="Добавить в избранные"><span class="icon icon-heart h5"></span></button>
-                    @endif
-
-                    @if (is_array($items) AND isset($items['products_id'][$product->id]))
-                      <a href="/basket" class="btn btn-dark btn-compact m-10" data-toggle="tooltip" data-placement="top" title="Перейти в корзину"><i class="icon icon-bag h5"></i></a>
-                    @else
-                      <button class="btn btn-primary btn-compact m-10 btn-sm" data-basket-id="{{ $product->id }}" onclick="addToBasket(this);" title="Добавить в корзину"><span class="icon icon-cart h5"></span></button>
-                    @endif
-                  </div>
-                </div>
-              @endforeach
+        <div class="row">
+          <div class="col-lg-6 col-sm-6 col-xs-6">
+            <h2 class="h1">{{ $mode->title }}</h2><br>
+          </div>
+          <div class="col-lg-6 col-sm-6 col-xs-6">
+            <div class="customNavigation xs-custom-nav h2 text-right">
+              <a class="prev" style="color: #941781;" href="#{{ $mode->slug }}Carousel" role="button" data-slide="prev"><b><i class="icon icon-left-arrows"></i></b></a>
+              <a class="next" style="color: #941781;" href="#{{ $mode->slug }}Carousel" role="button" data-slide="next"><b><i class="icon icon-right-arrow"></i></b></a>
             </div>
-          @endforeach
+          </div>
+        </div>
+        <div class="carousel slide" id="{{ $mode->slug }}Carousel" data-ride="carousel" data-interval="false">
+          <div class="carousel-inner">
+            @foreach($mode->products->where('status', 1)->take(16)->chunk(8) as $key => $chunk)
+              <div class="carousel-item @if($key == '0') active @endif">
+                <div class="row">
+                  @foreach($chunk as $product)
+                    <div class="col-lg-3 col-md-6 col-xs-6">
+                      <div class="xs-product-wraper version-3 text-center">
+                        <a href="/goods/{{ $product->id.'-'.$product->slug }}">
+                          <img src="/img/products/{{ $product->path.'/'.$product->image }}" alt="{{ $product->title }}">
+                        </a>
+                        <div class="offer">
+                          @foreach($product->modes as $m)
+                            @if(in_array($m->slug, ['novelty', 'best-price', 'stock', 'plus-gift']))
+                              <div class="offer-{{ $m->slug }}">{{ $m->title }}</div>
+                            @endif
+                          @endforeach
+                        </div>
+                        <div class="xs-product-content">
+                          <span class="product-categories">
+                            <a href="/catalog/{{ $mode->slug }}" rel="tag">{{ $mode->title }}</a>
+                          </span>
+                          <h4 class="product-title"><a href="/goods/{{ $product->id.'-'.$product->slug }}">{{ $product->title }}</a></h4>
+                          <span class="price"><b>{{ $product->price }}〒</b> <del></del></span>
+
+                          @if (is_array($favorites) AND in_array($product->id, $favorites['products_id']))
+                            <button type="button" class="btn btn-dark btn-compact m-10 btn-sm" data-favorite-id="{{ $product->id }}" onclick="toggleFavorite(this);" title="Добавлено в избранные"><span class="icon icon-heart h5"></span></button>
+                          @else
+                            <button type="button" class="btn btn-outline-primary btn-compact m-10 btn-sm" data-favorite-id="{{ $product->id }}" onclick="toggleFavorite(this);" title="Добавить в избранные"><span class="icon icon-heart h5"></span></button>
+                          @endif
+
+                          @if (is_array($items) AND in_array($product->id, $items['products_id']))
+                            <a href="/basket" class="btn btn-dark btn-compact m-10" data-toggle="tooltip" data-placement="top" title="Перейти в корзину"><i class="icon icon-bag h5"></i></a>
+                          @else
+                            <button class="btn btn-primary btn-compact m-10 btn-sm" data-basket-id="{{ $product->id }}" onclick="addToBasket(this);" title="Добавить в корзину"><span class="icon icon-cart h5"></span></button>
+                          @endif
+                        </div>
+                      </div>
+                    </div>
+                  @endforeach
+                </div>
+              </div>
+            @endforeach
+          </div>
         </div>
       </div>
     </section>
   @endforeach
+
 
   <!-- PRODUCTS LIST 2 -->
   <section class="xs-section-padding">
@@ -160,9 +180,9 @@
               @foreach($categories_part as $k => $category_item)
                 <li class="nav-item">
                   <a class="nav-link @if($k == '0') active @endif" id="{{ $category_item->slug }}-tab" data-toggle="tab" href="#today-{{ $category_item->slug }}" role="tab" aria-controls="today-{{ $category_item->slug }}" aria-selected="true">{{ $category_item->title }}</a>
-                  <div class="customNavigation xs-custom-nav">
-                    <a class="prev" href="#{{ $category_item->slug }}Carousel" role="button" data-slide="prev"><i class="icon icon-left-arrows"></i></a>
-                    <a class="next" href="#{{ $category_item->slug }}Carousel" role="button" data-slide="next"><i class="icon icon-right-arrow"></i></a>
+                  <div class="customNavigation xs-custom-nav h2 text-right">
+                    <a class="prev" style="color: #941781;" href="#{{ $category_item->slug }}Carousel" role="button" data-slide="prev"><i class="icon icon-left-arrows"></i></a>
+                    <a class="next" style="color: #941781;" href="#{{ $category_item->slug }}Carousel" role="button" data-slide="next"><i class="icon icon-right-arrow"></i></a>
                   </div>
                 </li>
               @endforeach
